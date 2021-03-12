2ec9f3661900df2bc3fb380c5ea79a9ce417c0a8
Rutwik-Kikani <kikanirutwik001@gmail.com>
2021-03-12 09:54:30 +0530

    file loader not working properly

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
 import Header from "./Header/Header";
 
 function App() {
-  return (
-    // <div className="container-fluid">
-    <Header />
-    // </div>
-  );
+  return <Header />;
 }
 
 export default App;
