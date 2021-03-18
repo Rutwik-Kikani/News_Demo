a6c30ada973e1f22f12535051548bfee04ca7ed9
Rutwik-Kikani <kikanirutwik001@gmail.com>
2021-03-18 11:12:23 +0530

    rearrange some files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+
+//imoprt styles
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router } from "react-router-dom";
+
 //components import
 // import configureStore from "./redux/configureStore";
 import App from "./components/App";
-import { Provider as ReduxProvider } from "react-redux";
 
 // const store = configureStore();
 
